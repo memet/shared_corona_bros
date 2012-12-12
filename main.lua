2a14d06afc6193a95be195a679b48d9fb82724f4
Memet <koslumemet@gmail.com>
2012-12-12 20:56:12 +0200

    last one

---
+++ main.lua
@@ -0,0 +1,14 @@
+local dortgen = display.newRect(70,130,350,100)
+local daire=display.newCircle(300,300,100)
+local yazi=display.newText("Zaman",50,150,native.systemFont,100)
+dortgen:setFillColor(50,150,250)
+daire:setFillColor(200,250,200)
+yazi:setTextColor(200,50,70)
+local cizgi  = display.newLine(0,30,480,30)
+cizgi.width=6
+
+local g1=display.newGroup()
+g1:insert(1,dortgen)
+g1:insert(2,daire)
+g1:insert(3,yazi)
+g1.y=200
