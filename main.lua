77518fcc9bd7ab7414561017c7c2bf62658a8729
Memet <koslumemet@gmail.com>
2012-12-12 22:21:59 +0200

    commiting version for testing

--- main.lua
+++ main.lua
@@ -5,10 +5,10 @@ dortgen:setFillColor(50,150,250)
 daire:setFillColor(200,250,200)
 yazi:setTextColor(200,50,70)
 local cizgi  = display.newLine(0,30,480,30)
-cizgi.width=6
+cizgi.width=4
 
 local g1=display.newGroup()
-g1:insert(1,dortgen)
+g1:insert(3,dortgen)
 g1:insert(2,daire)
-g1:insert(3,yazi)
+g1:insert(1,yazi)
 g1.y=200
