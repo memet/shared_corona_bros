8390cadc0287d8289e12c6a27c8ce8df98d56c5f
Memet <koslumemet@gmail.com>
2012-12-13 11:05:04 +0200

    simple start

--- main.lua
+++ main.lua
@@ -1,14 +1,4 @@
-local dortgen = display.newRect(70,130,350,100)
-local daire=display.newCircle(300,300,100)
-local yazi=display.newText("Zaman",50,150,native.systemFont,100)
-dortgen:setFillColor(50,150,250)
-daire:setFillColor(200,250,200)
-yazi:setTextColor(200,50,70)
-local cizgi  = display.newLine(0,30,480,30)
-cizgi.width=4
-
-local g1=display.newGroup()
-g1:insert(3,dortgen)
-g1:insert(2,daire)
-g1:insert(1,yazi)
-g1.y=200
+local titleBackground=display.newRect(50,50,200,50)
+titleBackground:setFillColor(200,200,200)
+local title=display.newText("Tracker",70,55,native.systemFont,36)
+title:setTextColor(0,0,0)
