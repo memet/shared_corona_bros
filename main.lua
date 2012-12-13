d32578d04099e51cf833badc81289b092b5d968b
Memet <koslumemet@gmail.com>
2012-12-14 01:10:19 +0200

    a button

--- main.lua
+++ main.lua
@@ -1,4 +1,19 @@
 local titleBackground=display.newRect(50,50,200,50)
 titleBackground:setFillColor(200,200,200)
-local title=display.newText("Tracker",70,55,native.systemFont,36)
+local title=display.newText("Tracker",70,55,nil,36)
 title:setTextColor(0,0,0)
+
+
+local widget=require "widget"
+local function onButtonRelease(event)
+	local btn=event.target
+	print("user has pressed and released the button")
+	btn:setLabel("doh! not yet !")
+end
+local button=widget.newButton{
+	label="My Location",
+	fontSize=20,
+	onRelease=onButtonRelease
+}
+button.x=display.contentWidth*0.5
+button.y=display.contentHeight*0.5
